e363b82695a3002c3984dc0491c8bbcc2c197be0
Adel Mahjoub <dragonhunter250@gmail.com>
2017-06-13 03:45:55 +0200

    distance pipe convert to miles

--- src/app/utils/distance.pipe.ts
+++ src/app/utils/distance.pipe.ts
@@ -4,9 +4,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'distance'
 })
 export class DistancePipe implements PipeTransform {
-
-  transform(distance: number): number {
-    return ~~(distance / 1000)
+  
+  transform(distance: number): string {
+    let language = navigator.language;
+    if(language === 'fr') return ~~(distance / 1000) + 'Km';
+    else return ~~(distance / 1609) + 'Mi'
   }
 
 }
